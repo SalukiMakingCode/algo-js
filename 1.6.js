ada2b612f9e46ebfd32f33de5a5d49e1f8e0812d
David Van Goidtsnoven <admin@spiderwolf.net>
2021-09-09 11:57:27 +0200

    1.6

--- 1.6.js
+++ 1.6.js
@@ -1,1 +1,4 @@
 const readlineSync = require("readline-sync");
+let num1 = Number (readlineSync.question("What's your first number ?"));
+let num2 = Number (readlineSync.question("What's your second number ?"));
+console.log(num1 % num2);
