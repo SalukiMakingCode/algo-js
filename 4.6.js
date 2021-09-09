3b6c15fb9ddf922b75dc7e5a5270420b34e5a686
David Van Goidtsnoven <admin@spiderwolf.net>
2021-09-09 14:49:16 +0200

    4.6

---
+++ 4.6.js
@@ -0,0 +1,10 @@
+const readlineSync = require("readline-sync");
+function factorial(a)
+{
+    if (a === 0)
+    {
+        return 1;
+    }
+    return a * factorial(a-1);
+}
+console.log(factorial(4));
