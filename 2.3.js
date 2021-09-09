98ab045f62f89b56e3b7ee82fcc42b6b8571faa2
David Van Goidtsnoven <admin@spiderwolf.net>
2021-09-09 13:43:08 +0200

    2.3

---
+++ 2.3.js
@@ -0,0 +1,11 @@
+const readlineSync = require("readline-sync");
+let i=1;
+//while (i<101)
+//{
+//    if (i%2===0) console.log(i);
+//    i++;
+//}
+
+for (i=2; i<101; i+=2) {
+    console.log(i);
+}
