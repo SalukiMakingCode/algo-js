aef8eab66393367b597158f2f13f772dd4edb100
David Van Goidtsnoven <admin@spiderwolf.net>
2021-09-09 11:40:19 +0200

    1.2

---
+++ 1.2.js
@@ -0,0 +1,5 @@
+const readlineSync = require("readline-sync");
+let name = "Van Goidtsnoven";
+let firstName = "David";
+let city = "Charleroi";
+console.log("Your name is" + firstName + " " + name + " and you live in " + city);
